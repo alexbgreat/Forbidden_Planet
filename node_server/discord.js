538a58f29602ee5c76de284751cafc274a055b1f
Alex O'Neill <alexbgreat@gmail.com>
2019-06-16 03:49:24 -0500

    Add some testing, begin separating duties

--- node_server/discord.js
+++ node_server/discord.js
@@ -1,5 +1,9 @@
 const { Client, RichEmbed } = require('discord.js');
+const Event = require('event.js');
+const Server = require('factorio.js');
+
 const client = new Client();
+const Server = new Server();
 
 const auth = require('./auth')
 
@@ -10,24 +14,8 @@ const escapeString = message => {
   return escaped
 }
 
-class Event{
-  constructor(){
-    this.events = {}
-  }
-
-  register(name, callback){
-    this.events[name] = callback
-  }
-
-  trigger(name){
-    console.log(`trigger: ${name}`)
-    if(this.events[name])
-      this.events[name]()
-  }
-}
 
 const event = new Event()
-
 
 client.login(auth.token);
 
@@ -118,121 +106,6 @@ setInterval(() => chatMessage("bananas"), 2500)
 */
 const { spawn, exec } = require('child_process');
 const readline = require('readline');
-class Server {
-  constructor(opts) {
-
-    this.process = undefined
-    this.options = Object.assign({
-      launch: {
-        path: '/opt/factorio/bin/x64/factorio',
-        latest: '--start-server-load-latest',
-        new: '--start-server-load-scenario',
-        name: 'forbidden_planet',
-        config: '/opt/factorio/scenarios/forbidden_planet/server-settings.json'
-      },
-    }, opts)
-  }
-
-  start(param = "latest") {
-    return new Promise((resolve, reject) => {
-      if(!param) reject("no start param")
-      const { options } = this
-      if(param == "new"){
-        this.process = spawn(options.launch.path, [options.launch.new, options.launch.name, "--server-settings", options.launch.config])
-        this._set_triggers(this.process)
-      }else if(param == "latest"){
-        this.process = spawn(options.launch.path, [options.launch.latest, "--server-settings", options.launch.config])
-        this._set_triggers(this.process)
-      }else{
-        reject("unknown param")
-      }
-      resolve("started")
-    })
-  }
-
-  stop() {
-    return new Promise((resolve, reject) => {
-      if(!this.process){
-        messageEmbedded("bananas", "Error", "Server is currently not running", 0xff0000)
-        reject("server not running")
-        return
-      }
-      this.process.kill('SIGHUP')
-      resolve("stopped")
-    })
-  }
-
-  online_players(){
-    if(!this.process) return
-    this.process.stdin.write(`/silent-command log("[ONLINE] " ..#game.connected_players)\n`)
-  }
-
-  restart() { }
-  
-  message(user, text) {
-    if(!this.process) return
-    this.process.stdin.write(`/silent-command game.print("[Discord] ${user}: ${text}", { r = 0.4, g = 0.6, b = 1})\n`)
-  }
-
-  _set_triggers(factorio_process){
-
-    factorio_process.stdout.on("data", data => {
-      console.log(`${data}`)
-      const error_filter = /.*Error(.*)/
-      const error = error_filter.exec(`${data}`)
-
-      const started = /.*ServerMultiplayerManager.cpp:705: Matching server connection resumed.*/
-
-      const messageFilter = /.*\[CHAT\]\s?(.*)/
-      const onlinePlayersFilter = /.*Script log.*\[ONLINE\]\s?(\d+)/
-      const joinFilter = /.*\[JOIN\]\s?(.*)/
-      const leaveFilter = /.*\[LEAVE\]\s?(.*)/
-      const join = joinFilter.exec(`${data}`)
-      const leave = leaveFilter.exec(`${data}`)
-      const message = messageFilter.exec(`${data}`)
-      const online = onlinePlayersFilter.exec(`${data}`)
-
-      if(error)
-       messageEmbedded("bananas", "Error", statusMessages.error.replace('__ERROR__', error[1]), 0xff0000)
-
-      if(started.test(data))
-        event.trigger("started")
-
-      if(join){
-        const user = join[1].split(" ")[0]
-        joinOrLeaveMessage("bananas", user, "joined")
-      }else if(leave){
-        const user = leave[1].split(" ")[0]
-        joinOrLeaveMessage("bananas", user, "left")
-      }else if(message){
-        const data = message[1].split(" ")
-        const user = data[0]
-        const text = data.slice(1, data.length)
-        chatMessage("bananas", user, text.join(" "))
-      }else if(online){
-        console.log("online")
-        messageEmbedded("bananas", "Status", statusMessages.online.replace('__ONLINE__', online[1]), 0x0000ff)
-      }
-
-    })
-
-    factorio_process.on('close', (code) => {
-      event.trigger("stop")
-    });
-    
-    factorio_process.on('error', (err) => {
-      console.log('Failed to start sub_process.');
-    });
-
-    process.on('SIGINT', async () => {
-      console.log("Caught interrupt signal");
-      factorio_process.kill('SIGHUP')
-      this.process = undefined
-      setTimeout(() => process.exit(), 1000)
-    });
-  
-  }
-}
 
 const server = new Server()
 
